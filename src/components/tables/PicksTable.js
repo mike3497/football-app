3f29f7c50dc07dc1f0dfff65d907d958048b8c04
Michael Carillon <mcarillon97@gmail.com>
2022-09-13 22:53:13 -0400

    Update Picks table

--- src/components/tables/PicksTable.js
+++ src/components/tables/PicksTable.js
@@ -1,30 +1,46 @@
-import React, { useEffect, useState, useContext, Fragment } from 'react';
-import { Card, Table, FloatingLabel, Form } from 'react-bootstrap';
-import { getPicks } from '../../services/picks-service';
+import React, { Fragment, useEffect, useState, useContext } from 'react';
 import AuthContext from '../../contexts/AuthContext';
+import { getGames } from '../../services/games-service';
+import { getPicks } from '../../services/picks-service';
+import { Card, Table, Form, FloatingLabel } from 'react-bootstrap';
 import PicksTableRow from './PicksTableRow';
 
 export default function PicksTable({ userId }) {
 	const authContext = useContext(AuthContext);
 
-	const [data, setData] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState(null);
+	const [gamesData, setGamesData] = useState([]);
+	const [gamesIsLoading, setGamesIsLoading] = useState(false);
+	const [gamesError, setGamesError] = useState(null);
+
+	const [picksData, setPicksData] = useState([]);
+	const [picksIsLoading, setPicksIsLoading] = useState(false);
+	const [picksError, setPicksError] = useState(null);
 
 	const [week, setWeek] = useState(getWeek());
 
 	useEffect(() => {
-		getData();
+		getGamesData();
+		getPicksData();
 	}, [week]);
 
-	async function getData() {
-		setIsLoading(true);
-		const data = await getPicks(authContext.token, userId, week);
+	async function getGamesData() {
+		setGamesIsLoading(true);
+		const data = await getGames(authContext.token, week);
 		if (data.error) {
 			return;
 		}
-		setData(data);
-		setIsLoading(false);
+		setGamesData(data);
+		setGamesIsLoading(false);
+	}
+
+	async function getPicksData() {
+		setPicksIsLoading(true);
+		const data = await getPicks(authContext.token, userId);
+		if (data.error) {
+			return;
+		}
+		setPicksData(data);
+		setPicksIsLoading(false);
 	}
 
 	function getWeek() {
@@ -80,57 +96,63 @@ export default function PicksTable({ userId }) {
 	return (
 		<Card>
 			<Card.Body>
-				<FloatingLabel className="mb-3" controlId="select-week" label="Week">
-					<Form.Select onChange={(e) => setWeek(e.target.value)} value={week}>
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-						<option value="4">4</option>
-						<option value="5">5</option>
-						<option value="6">6</option>
-						<option value="7">7</option>
-						<option value="8">8</option>
-						<option value="9">9</option>
-						<option value="10">10</option>
-						<option value="11">11</option>
-						<option value="12">12</option>
-						<option value="13">13</option>
-					</Form.Select>
-				</FloatingLabel>
+				<h3>Picks</h3>
 
-				{isLoading && (
-					<Fragment>
-						<div className="d-flex justify-content-center my-4">
-							<div className="spinner-border" role="status">
-								<span className="visually-hidden">Loading...</span>
+				<Fragment>
+					<FloatingLabel className="mb-3" controlId="select-week" label="Week">
+						<Form.Select onChange={(e) => setWeek(e.target.value)} value={week}>
+							<option value="1">1</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+							<option value="4">4</option>
+							<option value="5">5</option>
+							<option value="6">6</option>
+							<option value="7">7</option>
+							<option value="8">8</option>
+							<option value="9">9</option>
+							<option value="10">10</option>
+							<option value="11">11</option>
+							<option value="12">12</option>
+							<option value="13">13</option>
+						</Form.Select>
+					</FloatingLabel>
+
+					{gamesIsLoading && picksIsLoading && (
+						<Fragment>
+							<div className="d-flex justify-content-center my-4">
+								<div className="spinner-border" role="status">
+									<span className="visually-hidden">Loading...</span>
+								</div>
 							</div>
-						</div>
-						<div className="text-center">Loading...</div>
-					</Fragment>
-				)}
+							<div className="text-center">Loading...</div>
+						</Fragment>
+					)}
 
-				{!isLoading && data.length > 0 && (
-					<Fragment>
-						<Table striped bordered responsive>
+					{!gamesIsLoading && !picksIsLoading && gamesData.length > 0 && (
+						<Table striped bordered hover responsive>
 							<thead>
 								<tr>
-									<td>Date</td>
-									<td>Home</td>
-									<td>Away</td>
+									<th>Date</th>
+									<th>Home</th>
+									<th>Away</th>
 								</tr>
 							</thead>
 							<tbody>
-								{data.map((pick) => (
-									<PicksTableRow key={pick.id} pick={pick} />
+								{gamesData.map((game) => (
+									<PicksTableRow
+										key={game.id}
+										game={game}
+										pick={picksData.find((pick) => pick.gameId === game.id)}
+									/>
 								))}
 							</tbody>
 						</Table>
-					</Fragment>
-				)}
+					)}
 
-				{!isLoading && data.length === 0 && (
-					<p className="text-center mb-0">No picks found.</p>
-				)}
+					{!gamesIsLoading && !picksIsLoading && gamesData.length === 0 && (
+						<div className="text-center">No games found.</div>
+					)}
+				</Fragment>
 			</Card.Body>
 		</Card>
 	);
